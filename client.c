02411ebc5af0f9c4e2aca6391a0fc1598cc7af9e
MRyo-ie <k2085788@ie.u-ryukyu.ac.jp>
2020-08-27 20:56:17 +0900

    [ver0.12]「http リクエストを作成するクライアントを作った。」【リファクタリング】
    https サーバーへのアクセスは、なぜか弾かれた...。
    （:80 にしたのに、
    http://ie.u-ryukyu.ac.jp/:80 にアクセスします。
    connect: Connection refused
    ）

--- client.c
+++ client.c
@@ -9,12 +9,11 @@
 
 int main(int argc, char **argv) {
     int sockfd, nbytes;
+    char *host = "127.0.0.1";
+    int port = 10000;
+    char *path = "/";
+    char http_msg_line[256];
     char buf[BUFSIZ];
-    char *host;
-    int port = 80;
-    // int port = 10000;
-    char *path;
-    char *http_msg;
     struct sockaddr_in servaddr;
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -23,36 +22,35 @@ int main(int argc, char **argv) {
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port   = htons(port);
-    // 引数でURLが指定されていたら
-    if (argc > 2) {
-        host = argv[1];
-        path = argv[2];
-    } else {
-        host = "ie.u-ryukyu.ac.jp";  // "10.3.4.7";
-        path = "/";
+    // 引数からpathを読み込む。（なければ初期値： "/"）
+    if (argc > 1) {
+        path = argv[1];
     }
-    printf("http://%s:%d%s にアクセスします。\n\n", host, port, path);
+    printf("http://%s:%d%s にHTTPリクエストします。\n\n", host, port, path);
     if (inet_pton(AF_INET, host, &servaddr.sin_addr) < 0) {
         perror("inet_pton"); exit(1);
     }
 
+    puts("httpリクエストを作成中...");
     // Connect
     if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
         perror("connect"); exit(1);
     }
     // Read / Write
-    sprintf(http_msg, "GET %s HTTP/1.0\r\n", path);
-    write(sockfd, http_msg, strlen(http_msg));
+    sprintf(http_msg_line, "GET %s HTTP/1.0\r\nHost: %s:%d\r\n\r\n", path, host, port);
+    puts(http_msg_line);
 
-    sprintf(http_msg, "Host: %s:%d\r\n", host, port);
-    printf(http_msg);
-    write(sockfd, http_msg, strlen(http_msg));
+    // sprintf(http_msg_line, "GET %s HTTP/1.0\r\n", path);
+    // puts(http_msg_line);
+    // write(sockfd, http_msg_line, strlen(http_msg_line)+1);
 
-    sprintf(http_msg, "\r\n");
-    write(sockfd, http_msg, strlen(http_msg));
+    // sprintf(http_msg_line, "Host: %s:%d\r\n", host, port);
+    // puts(http_msg_line);
+    // write(sockfd, http_msg_line, strlen(http_msg_line)+1);
 
-    // sprintf(http_msg, "GET %s HTTP/1.0\r\nHost: %s:%d\r\n\r\n", path);
-    nbytes = write(sockfd, http_msg, strlen(http_msg)+1);
+    // sprintf(http_msg_line, "\r\n");
+    write(sockfd, http_msg_line, strlen(http_msg_line)+1);
+
     nbytes = read(sockfd, buf, sizeof(buf)-1);
     buf[nbytes] = '\0';
     puts(buf);
